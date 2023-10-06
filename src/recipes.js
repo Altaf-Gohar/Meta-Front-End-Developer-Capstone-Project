f88d89ef21d04d680967f0940f1618119a5520d0
Altaf-Gohar <146984938+Altaf-Gohar@users.noreply.github.com>
2023-10-06 21:50:33 +0500

    Add files via upload

---
+++ src/recipes.js
@@ -0,0 +1,31 @@
+const recipes = [
+    { 
+      id: 1,
+      title: "Grilled Swordfish with Lemon",
+      price: 45.00,
+      image: "https://cockatoo.agency/wp-content/uploads/2023/10/Grilled-Swordfish-with-Lemon.png",
+      description:
+        "The fish is seared and cooked on the grill, either marinated in lemon before for an intense flavour or drizzled in Lemon juice after, for a more sauce-like effect.",
+    },
+    {
+      id: 2,
+      title: "Stuffed Vine Leaves",
+      price: 15.00,
+      image: "https://cockatoo.agency/wp-content/uploads/2023/10/Stuffed-Vine-Leaves.png",
+      description:
+        "Rice and tiny cubes of onion, with minced meat, are filled and neatly wrapped in little parcels using vine leaves, and eaten as a whole.",
+    },
+    {
+      id: 3,
+      title: "Shish Tawook",
+      price: 17.00,
+      image: "https://cockatoo.agency/wp-content/uploads/2023/10/Shish-Tawook.png",
+      description:
+        "The chicken meat is marinated in a mixture of Lemon juice, garlic and yogurt.It is then chopped into rough chunks and grilled on its skewer.",
+    },
+  
+  ];
+  
+  export default recipes;
+  
+  
